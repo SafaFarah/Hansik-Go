9a9dedb57f145fe93fd4bcd1b933a9a34857fb4e
SafaFarah <kangjisook13@gmail.com>
2026-08-07 21:37:57 +0300

    Set up food API with MongoDB and image uploads

---
+++ backend/controllers/foodController.js
@@ -0,0 +1,76 @@
+import FoodModel from "../models/FoodModel.js";
+import fs from 'fs';
+import foodRouter from "../routes/foodRoute.js";
+
+//add food item
+const addFood = async (req, res) => {
+
+  const imageFilename = req.file.filename;
+
+  const food = new FoodModel({
+    name: req.body.name,
+    image: imageFilename,
+    price: req.body.price,
+    description: req.body.description,
+    category: req.body.category
+  })
+
+  try {
+    await food.save();
+    res.json({ success: true, message: "Food added" })
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      success: false,
+      message: "Failed to add food.",
+    });
+  }
+}
+
+//foods list
+const listFood = async (req, res) => {
+  try {
+    const foods = await FoodModel.find({});
+    res.json({ success: true, data: foods })
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      success: false,
+      message: "Failed to retrieve food items.",
+    });
+  }
+}
+
+// remove food item
+const removeFood = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const food = await FoodModel.findById(id)
+    if (!food) {
+      return res.status(404).json({
+        success: false,
+        message: "Food not found.",
+      });
+    }
+
+    //remove image
+    fs.unlink(`uploads/${food.image}`, (err) => {
+      if (err) {
+        console.error(err);
+      }
+    });
+    
+    // delete food
+    await food.deleteOne();
+    res.json({ success: true, message: "Food removed" })
+    
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      success: false,
+      message: "Failed to remove food.",
+    });
+  }
+}
+
+export { addFood, listFood, removeFood }
