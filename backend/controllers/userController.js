98ec1afb17e014946f251282f8cef2c3cddb7ae0
SafaFarah <kangjisook13@gmail.com>
2026-09-14 17:29:08 +0300

    feat: add user registration flow

---
+++ backend/controllers/userController.js
@@ -0,0 +1,65 @@
+import UserModel from '../models/userModel.js';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+import validator from 'validator';
+
+
+// login user
+const loginUser = async (req, res) => {
+
+}
+
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '1d' });
+};
+
+
+// register user
+const registerUser = async (req, res) => {
+
+  const { name, email, password } = req.body;
+
+  try {
+    // Validate required fields
+    if (!name || !email || !password) {
+      return res.status(400).json({
+        success: false,
+        message: "Name, email, and password are required."
+      });
+    }
+
+    // Check if user already exists
+    const existingUser = await UserModel.findOne({ email });
+    if (existingUser) {
+      return res.status(409).json({ success: false, message: "User already exists." });
+    }
+
+    // Validate email and password
+    if (!validator.isEmail(email)) {
+      return res.status(400).json({ success: false, message: "Please enter a valid email." });
+    }
+
+    if (!validator.isStrongPassword(password)) {
+      return res.status(400).json({ success: false, message: "Password must be at least 8 characters and include uppercase, lowercase, number, and special character(!, @, #, $)." });
+    }
+
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Create a new user
+    const newUser = new UserModel({
+      name,
+      email,
+      password: hashedPassword
+    });
+
+    const user = await newUser.save();
+    const token = generateToken(user._id);
+    return res.status(201).json({ success: true, token });
+  } catch (error) {
+    console.error("Error registering user:", error);
+    return res.status(500).json({ success: false, message: "Error registering user." });
+  }
+}
+
+export { loginUser, registerUser };
